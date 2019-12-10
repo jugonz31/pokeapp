02f0ade8cc3944a9d9c2fb3e980d2a2bd1c3a4b5
Juan Pablo Gonzalez Alvarez <jugonz31@eafit.edu.co>
2019-12-10 16:22:41 -0500

    Using react-router for navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import PokemonList from './components/PokemonList';
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
 import store from "./store"
 
 function App() {
@@ -13,8 +14,18 @@ function App() {
 
   return (
     <Provider store={store}>
-      <Navbar onChange={handleChange} />
-      <PokemonList searchInput={searchInput}/>
+      <Switch>
+        <Route exact path="/">
+          <Navbar onChange={handleChange} />
+          <PokemonList searchInput={searchInput} />
+        </Route>
+        <Route path="/items">
+          <h1 className="ml-4">About</h1>
+        </Route>
+        <Route path="*">
+          <h2 className="mx-2">Page not found</h2>
+        </Route>
+      </Switch>
     </Provider>
   )
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand, Nav, NavItem, Form, FormGroup, Input} from 'reactstrap';
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink, Form, FormGroup, Input } from 'reactstrap';
 
-class NavbarComponent extends Component {    
+class NavbarComponent extends Component {
     render() {
         return (
-                <Navbar color="light" light sticky="top">
-                    <NavbarBrand href="/">PokéApp</NavbarBrand>
-                    <Nav>
-                        <NavItem>
-                            <Form inline>
-                                <FormGroup>
-                                     <Input type="search" name="search" id="searchInput" 
-                                     placeholder="Search" className="mr-auto" onChange={this.props.onChange}/>
-                                </FormGroup>
-                            </Form>
-                        </NavItem>
-                    </Nav>
-                </Navbar>
+            <Navbar color="light" light sticky="top">
+                <NavbarBrand href="/">PokéApp</NavbarBrand>
+                <Nav className="mr-auto">
+                    <NavItem>
+                        <NavLink light href="/about">About</NavLink>
+                    </NavItem>
+                </Nav>
+                <Form inline>
+                    <FormGroup>
+                        <Input type="search" name="search" id="searchInput"
+                            placeholder="Search" className="mr-auto" onChange={this.props.onChange} />
+                    </FormGroup>
+                </Form>
+            </Navbar>
         );
     }
 }
