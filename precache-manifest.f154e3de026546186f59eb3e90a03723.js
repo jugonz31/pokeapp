fedebfd20fd057d7dcbba4f732741e79d06258d9
Juan Pablo Gonzalez Alvarez <jugonz31@eafit.edu.co>
2019-12-12 09:20:09 -0500

    Updates

---
+++ precache-manifest.f154e3de026546186f59eb3e90a03723.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e60e5bdb443ec79c3b58028399e9056c",
+    "url": "/pokeapp/index.html"
+  },
+  {
+    "revision": "94307f43ebb690837dd4",
+    "url": "/pokeapp/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "0443151b22efd8b49b7f",
+    "url": "/pokeapp/static/css/main.95ad77f8.chunk.css"
+  },
+  {
+    "revision": "94307f43ebb690837dd4",
+    "url": "/pokeapp/static/js/2.730007ec.chunk.js"
+  },
+  {
+    "revision": "0443151b22efd8b49b7f",
+    "url": "/pokeapp/static/js/main.07b6c008.chunk.js"
+  },
+  {
+    "revision": "7d7189e3ae7ad859c861",
+    "url": "/pokeapp/static/js/runtime-main.df182e31.js"
+  }
+]);
